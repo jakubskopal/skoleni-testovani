54d5096878251ce3b3fb65f31cd462ccb3c05983
Jakub Skopal <j@kubs.cz>
2016-02-14 21:44:57 +0100

    unit tests pass on included Angular front-end

---
+++ tests/karma.conf.js
@@ -0,0 +1,20 @@
+module.exports = function (config) {
+	'use strict';
+
+	config.set({
+		basePath: '../',
+		frameworks: ['jasmine'],
+		files: [
+			'node_modules/angular/angular.js',
+			'node_modules/angular-route/angular-route.js',
+			'node_modules/angular-mocks/angular-mocks.js',
+			'node_modules/angular-resource/angular-resource.js',
+			'js/**/*.js',
+			'tests/unit/**/*.js'
+		],
+		autoWatch: true,
+		singleRun: true,
+		browsers: ['Chrome', 'Firefox']
+	});
+};
+
--- tests/protractor.conf.js
+++ tests/protractor.conf.js
@@ -4,6 +4,6 @@
  */
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['todo-spec.js']
+  specs: ['tests/system/todo-spec.js']
 };
 
